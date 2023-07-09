8709a1e8df924fffc7f32a20a0bc3e7bf5dcd695
Daniel Brühl <bruehl.dev@gmail.com>
2023-07-09 17:57:10 +0200

    feat(embeddings): adjust to model service class

--- src/embeddings/router.py
+++ src/embeddings/router.py
@@ -1,17 +1,14 @@
 from fastapi import APIRouter, Depends
 from bertopic import BERTopic
-from models.service import load_model
+
+from models.service import ModelService
 from data.schemas import Dataset_names, Experimental_dataset_names
+from models.schemas import Model_names
 from embeddings.service import get_embeddings
 
 router = APIRouter()
 
 
-@router.get("/load_model/{dataset_name}")
-def load_model_endpoint(dataset_name: Experimental_dataset_names, model: BERTopic = Depends(load_model)):
-    return {"message": f"{dataset_name} dataset loaded successfully"}
-
-
-@router.get("/embeddings/{dataset_name}")
-def get_embeddings_endpoint(dataset_name: Experimental_dataset_names, embeddings: list = Depends(get_embeddings)):
-    return {"embeddings": embeddings}
+@router.get("/data/{dataset_name}/model/{model_name}/embeddings")
+def get_embeddings_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names):
+    return {"embeddings": get_embeddings(dataset_name, model_name)}
