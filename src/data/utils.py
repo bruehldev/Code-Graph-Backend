9fa783ba9731ecd83dd73fda8e818699d1e17e26
Daniel Brühl <bruehl.dev@gmail.com>
2023-08-17 15:48:54 +0200

    feat(data.utils): implement model_name on paths

--- src/data/utils.py
+++ src/data/utils.py
@@ -10,14 +10,19 @@ def get_root_path(type: str, dataset_name: str):
     return os.path.join(env["exported_folder"], type, dataset_name)
 
 
-def get_supervised_path(type: str, dataset_name: str):
-    return os.path.join(get_root_path(type, dataset_name), "supervised")
+def get_supervised_path(type: str, dataset_name: str, model_name=None):
+    if model_name is None:
+        return os.path.join(get_root_path(type, dataset_name), "supervised")
+    else:
+        return os.path.join(get_root_path(type, dataset_name), "supervised", model_name[:12])
 
 
-def get_path_key(type: str, dataset_name: str):
-    return os.path.join(type, dataset_name, "supervised").replace("/", "_")
+def get_path_key(type: str, dataset_name: str, model_name=None):
+    if model_name is None:
+        return os.path.join(type, dataset_name, "supervised").replace("/", "_")
+    else:
+        return os.path.join(type, dataset_name, "supervised", model_name[:12]).replace("/", "_")
 
 
-def get_file_path(type: str, dataset_name: str, filename: str):
-    data_file_path = os.path.join(get_supervised_path(type, dataset_name), filename)
-    return data_file_path
+def get_file_path(type: str, dataset_name: str, model_name: str, filename: str):
+    return os.path.join(get_supervised_path(type, dataset_name, model_name), filename)
