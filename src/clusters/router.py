3414bb916bf1d69e89aa7cb84e7d3ede78ad82cd
Daniel Brühl <bruehl.dev@gmail.com>
2023-08-21 19:38:43 +0200

    feat(cluster): implement db and crud operations

--- src/clusters/router.py
+++ src/clusters/router.py
@@ -1,15 +1,56 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+
+from pydantic import BaseModel
+
 from data.schemas import Experimental_dataset_names
 from clusters.service import get_clusters, extract_clusters
 from models.schemas import Model_names
-
+from database.postgresql import ClustersTable, get as get_from_db, get_data as get_data_db, create, update, delete
+from data.utils import get_path_key
+from clusters.schemas import Cluster
 
 router = APIRouter()
 
 
+class ClustersTableResponse(BaseModel):
+    id: int
+    cluster: int
+
+
 @router.get("/")
-def get_clusters_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, page: int = 1, page_size: int = 100):
+def list_clusters_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, page: int = 1, page_size: int = 100):
     return {"clusters": get_clusters(dataset_name, model_name, start=(page - 1) * page_size, end=page * page_size)}
+
+
+@router.get("/{id}", response_model=ClustersTableResponse)
+def get_cluster_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, id: int):
+    table_name = get_path_key("clusters", dataset_name, model_name)
+    data_from_db = get_from_db(table_name, ClustersTable, id)
+
+    if data_from_db is not None:
+        return ClustersTableResponse(**data_from_db.__dict__)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
+
+
+@router.post("/", response_model=Cluster)
+def insert_cluster_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, data: Cluster = {"cluster": -1}):
+    table_name = get_path_key("clusters", dataset_name, model_name)
+    create(table_name, ClustersTable, cluster=data.cluster)
+    return data.__dict__
+
+
+@router.put("/{id}", response_model=Cluster)
+def update_cluster_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, id: int, data: Cluster = {"cluster": -2}):
+    table_name = get_path_key("clusters", dataset_name, model_name)
+    update(table_name, ClustersTable, id, data.dict())
+    return data
+
+
+@router.delete("/{id}")
+def delete_cluster_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, id: int):
+    table_name = get_path_key("clusters", dataset_name, model_name)
+    return {"id": id, "deleted": delete(table_name, ClustersTable, id)}
 
 
 @router.get("/extract")
--- src/clusters/service.py
+++ src/clusters/service.py
@@ -1,10 +1,9 @@
 import json
-import os
-import json
 import logging
-from fastapi import Depends
 from typing import List
-from reduced_embeddings.service import get_reduced_embeddings
+from embeddings.service import get_embeddings
+from database.postgresql import ClustersTable, table_has_entries, get_data as get_data_db, init_table, create
+from data.utils import get_path_key
 
 import logging
 import numpy as np
@@ -23,49 +22,29 @@ config_manager = ConfigManager(env["configs"])
 config = config_manager.get_default_model()
 
 
-def get_clusters_file(dataset_name: str):
-    clusters_directory = os.path.join(env["clusters_path"], dataset_name)
-    os.makedirs(clusters_directory, exist_ok=True)
-    return os.path.join(clusters_directory, f"clusters_{dataset_name}.json")
+def save_clusters(clusters: np.ndarray, dataset_name: str, model_name: str):
+    logger.info(f"Save clusters db: {dataset_name} / {model_name}. Length: {len(clusters)}")
+    path_key = get_path_key(type="clusters", dataset_name=dataset_name, model_name=model_name)
+    init_table(path_key, ClustersTable)
 
-
-def save_clusters(clusters: np.ndarray, file_name: str):
-    with open(file_name, "w") as f:
-        json.dump(clusters, f)
-
-
-def load_clusters(file_name: str) -> np.ndarray:
-    with open(file_name, "r") as f:
-        clusters_list = json.load(f)
-        return clusters_list
+    for cluster in clusters:
+        create(path_key, ClustersTable, cluster=int(cluster))
 
 
 def get_clusters(dataset_name: str, model_name: str, start: int = 0, end: int = None):
-    logger.info(f"Getting clusters for dataset: {dataset_name}")
-
-    clusters_file = get_clusters_file(dataset_name)
-
-    if os.path.exists(clusters_file):
-        clusters = load_clusters(clusters_file)
-        logger.info(f"Loaded clusters from file for dataset: {dataset_name}")
-        clusters = eval(clusters)
-        # TODO Fix clusters not being a list but a string
+    path_key = get_path_key(type="clusters", dataset_name=dataset_name, model_name=model_name)
+    if table_has_entries(path_key, ClustersTable):
+        clusters = get_data_db(path_key, start, end, ClustersTable)
+        return clusters
     else:
         clusters = extract_clusters(dataset_name, model_name)
-
-    return clusters[start:end]
+        save_clusters(clusters, dataset_name, model_name)
+        return clusters[start:end]
 
 
 def extract_clusters(dataset_name: str, model_name: str):
-    clusters_file = get_clusters_file(dataset_name)
     clusterer = hdbscan.HDBSCAN(**config.cluster_config.dict())
-    clusters = clusterer.fit_predict(get_reduced_embeddings(dataset_name, model_name))
-    logger.info(f"Computed clusters for dataset: {dataset_name} with length: {len(clusters)}")
-    # convert the clusters to a JSON serializable format
-    clusters = [int(c) for c in clusterer.labels_]
-    # serialize the clusters to JSON
-    json_clusters = json.dumps(clusters)
-    save_clusters(json_clusters, clusters_file)
-    logger.info(f"Computed and saved clusters for dataset: {dataset_name}")
-    clusters = list(clusters)
+    clusters = clusterer.fit_predict(get_embeddings(dataset_name, model_name))
+    logger.info(f"Computed clusters: {dataset_name} / {model_name}")
+    save_clusters(clusters, dataset_name, model_name)
     return clusters
