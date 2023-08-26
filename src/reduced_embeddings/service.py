59594abb9d60ba8ca4165918d942b3dbe21777b7
Daniel Brühl <bruehl.dev@gmail.com>
2023-08-26 21:33:22 +0200

    refactor(reduced_embeddings.service): clean code improve extract return

--- src/reduced_embeddings/service.py
+++ src/reduced_embeddings/service.py
@@ -33,7 +33,6 @@ config_manager = ConfigManager(env["configs"])
 config = config_manager.get_default_model()
 
 
-# Keeping file operations for now
 ### Reduced Embedding functions ###
 def get_reduced_embeddings(dataset_name: str, model_name: str, start=0, end=None):
     reduced_embedding_table_name = get_path_key("reduced_embedding", dataset_name, model_name)
@@ -44,15 +43,10 @@ def get_reduced_embeddings(dataset_name: str, model_name: str, start=0, end=None
 
     if table_has_entries(reduced_embeddings_table):
         embeddings_reduced = get_all_db(reduced_embeddings_table, start, end, True)
-        return embeddings_reduced
-        # return [row.__dict__ for row in data]
-        # embeddings_reduced = load_reduced_embeddings(dataset_name, model_name, start, end)
     else:
         embeddings_reduced = extract_embeddings_reduced(dataset_name, model_name, start, end)
 
-        if isinstance(embeddings_reduced, np.ndarray):
-            embeddings_reduced = embeddings_reduced.tolist()
-        return embeddings_reduced
+    return embeddings_reduced
 
 
 def save_reduced_embeddings(reduced_embeddings: np.ndarray, index_list: List[int], dataset_name: str, model_name: str):
@@ -70,14 +64,7 @@ def save_reduced_embeddings(reduced_embeddings: np.ndarray, index_list: List[int
         update_or_create_db(session, reduced_embeddings_table, data_id=segment_id, reduced_embeddings=embedding.tolist())
 
 
-def get_reduced_embeddings_file(dataset_name: str, model_name: str):
-    embeddings_directory = get_supervised_path("embeddings", dataset_name, model_name)
-    os.makedirs(embeddings_directory, exist_ok=True)
-    return get_model_file_path(type="embeddings", dataset_name=dataset_name, model_name=model_name, filename=f"reduced_embeddings_{dataset_name}.pkl")
-
-
 def extract_embeddings_reduced(dataset_name, model_name, start=0, end=None):
-    # embeddings with index
     # Important Note: Always use all embeddings for UMAP to optimize the embedding space
     embeddings_with_index = get_embeddings(dataset_name, model_name, start=0, end=None, with_index=True)
 
@@ -95,10 +82,19 @@ def extract_embeddings_reduced(dataset_name, model_name, start=0, end=None):
 
     save_reduced_embeddings(embeddings_reduced, index_list, dataset_name, model_name)
     logger.info(f"Computed reduced embeddings: {dataset_name} / {model_name}")
-    return embeddings_reduced[start:end]
+
+    # transform to list of dict with id in start and end range
+    embeddings_reduced = [[index, embedding] for index, embedding in zip(index_list[start:end], embeddings_reduced.toList()[start:end])]
+    return embeddings_reduced
 
 
-### Save load reduced embeddings from pickle file and not db ###
+### operations for file and not db ###
+def get_reduced_embeddings_file(dataset_name: str, model_name: str):
+    embeddings_directory = get_supervised_path("embeddings", dataset_name, model_name)
+    os.makedirs(embeddings_directory, exist_ok=True)
+    return get_model_file_path(type="embeddings", dataset_name=dataset_name, model_name=model_name, filename=f"reduced_embeddings_{dataset_name}.pkl")
+
+
 def save_reduced_embeddings_pickle(reduced_embeddings: np.ndarray, dataset_name: str, model_name: str):
     embeddings_file = get_reduced_embeddings_file(dataset_name, model_name)
     with open(embeddings_file, "wb") as f:
