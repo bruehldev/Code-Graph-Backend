abc4a0219d2beb44b329daf2fe999f62ee55b375
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-14 21:20:18 +0200

    feat(embeddings.router): add new db

--- src/embeddings/router.py
+++ src/embeddings/router.py
@@ -1,6 +1,7 @@
 import pickle
+import json
 from typing import List, Optional
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 
 from database.postgresql import get as get_in_db, create as create_in_db, update as update_in_db, delete as delete_in_db, get_embedding_table
 from embeddings.service import get_embeddings, extract_embeddings
@@ -8,7 +9,13 @@ from embeddings.service import get_embeddings, extract_embeddings
 from data.schemas import Experimental_dataset_names
 from models.schemas import Model_names
 from embeddings.schemas import EmbeddingTable, EmbeddingEntry, DataEmbeddingResponse, EmbeddingData
-from database.schemas import DeleteResponse
+from db.schemas import DeleteResponse
+from db.models import Segment, Sentence, Dataset, Project, Embedding
+
+from models_neu.model_definitions import MODELS
+from configmanager.service import ConfigManager
+from db.session import get_db
+from sqlalchemy import not_, and_, exists
 
 from data.utils import get_path_key
 
@@ -35,16 +42,54 @@ def get_embeddings_endpoint(
 
 @router.get("/extract")
 def extract_embeddings_endpoint(
-    dataset_name: Experimental_dataset_names,
-    model_name: Model_names,
-    all: bool = False,
     page: int = 1,
     page_size: int = 100,
+    project_id: int = None,
     id=None,
     reduce_length: int = 3,
     return_data: bool = False,
-) -> EmbeddingTable:
+    db=Depends(get_db),
+):
     embeddings = []
+    config_manager = ConfigManager(db)
+    config = config_manager.get_project_config(project_id)
+    config_as_json = json.loads(config.config)
+    model_name = config_as_json["embedding_config"]["model_name"]
+    embedding_model = MODELS[model_name](config_as_json["embedding_config"]["args"])
+
+    subquery = exists().where(and_(Embedding.segment_id == Segment.segment_id, Embedding.model_id == 3))
+
+    segments_and_sentences = (
+        db.query(Segment, Sentence)
+        .join(Sentence, Sentence.sentence_id == Segment.sentence_id)
+        .join(Dataset, Dataset.dataset_id == Sentence.dataset_id)
+        .join(Project, Project.project_id == Dataset.project_id)
+        .filter(Project.project_id == project_id)
+        .filter(not_(subquery))
+        .all()
+    )
+    segments, sentences = zip(*segments_and_sentences)
+    embeddings = embedding_model.transform(segments, sentences)
+
+    ## saving
+
+
+"""
+    embedding_mappings = [
+        {"segment_id": segment.segment_id, "ModelID": db_e.ModelID, "EmbeddingValues": pickle.dumps(embedding_value)}
+        for embedding_value, segment in zip(embedding_values, segments)
+    ]
+
+    # Bulk insert embeddings
+    db.bulk_insert_mappings(models.Embedding, embedding_mappings)
+    db.commit()
+
+    print(embeddings[0])
+    return {"config": len(embeddings)}
+
+"""
+
+"""
     if all:
         embeddings = extract_embeddings(dataset_name, model_name)
         if return_data:
@@ -59,6 +104,7 @@ def extract_embeddings_endpoint(
             else:
                 return {"length": len(embeddings), "id": id, "data": embeddings, "reduce_length": reduce_length}
     return {"length": len(embeddings), "page": page, "page_size": page_size, "reduce_length": reduce_length, "data": []}
+"""
 
 
 def limit_embeddings_length(embeddings, reduce_length):
