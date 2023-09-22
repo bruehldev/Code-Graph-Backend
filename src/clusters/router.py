b4c505d71a70197546e35409a7dd549b277bc0d6
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-22 19:53:23 +0200

    refactor(clusters.router): remove unused code

--- src/clusters/router.py
+++ src/clusters/router.py
@@ -1,23 +1,11 @@
 import numpy as np
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy import and_, exists, not_
 from sqlalchemy.orm import Session
 
-from clusters.schemas import (ClusterData, ClusterEntry, ClusterTable,
-                              DataClusterResponse)
-from clusters.service import extract_clusters, get_clusters
-from data.schemas import Experimental_dataset_names
-from data.utils import get_path_key
-from database.postgresql import create as create_in_db
-from database.postgresql import delete as delete_in_db
-from database.postgresql import get as get_in_db
-from database.postgresql import get_cluster_table
-from database.postgresql import update as update_in_db
 from db.models import Cluster, Model, Project, ReducedEmbedding
-from db.schema import DeleteResponse
 from db.session import get_db
-from models.schemas import Model_names
 from project.service import ProjectService
 
 router = APIRouter()
@@ -87,48 +75,3 @@ def get_clusters_endpoint(project_id: int, all: bool = False, page: int = 1, pag
     return_dict.update({"length": len(clusters), "data": clusters})
 
     return return_dict
-
-
-@router.get("/{id}")
-def get_cluster_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, id: int) -> DataClusterResponse:
-    cluster_table_name = get_path_key("clusters", dataset_name, model_name)
-    segment_table_name = get_path_key("segments", dataset_name)
-    cluster_table = get_cluster_table(cluster_table_name, segment_table_name)
-    data = None
-    try:
-        data = get_in_db(cluster_table, id)
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"{str(e)}")
-    if data is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
-    return {"data": data}
-
-
-@router.put("/{id}")
-def update_cluster_endpoint(
-    dataset_name: Experimental_dataset_names, model_name: Model_names, id: int, data: ClusterData = {"cluster": -2}
-) -> DataClusterResponse:
-    cluster_table_name = get_path_key("clusters", dataset_name, model_name)
-    segment_table_name = get_path_key("segments", dataset_name)
-    cluster_table = get_cluster_table(cluster_table_name, segment_table_name)
-
-    response = None
-    try:
-        response = update_in_db(cluster_table, id, data.dict())
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"{str(e)}")
-    if response is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
-
-    return {"data": response}
-
-
-@router.delete("/{id}")
-def delete_cluster_endpoint(dataset_name: Experimental_dataset_names, model_name: Model_names, id: int) -> DeleteResponse:
-    cluster_table_name = get_path_key("clusters", dataset_name, model_name)
-    segment_table_name = get_path_key("segments", dataset_name)
-    cluster_table = get_cluster_table(cluster_table_name, segment_table_name)
-    try:
-        return {"id": id, "deleted": delete_in_db(cluster_table, id)}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"{str(e)}")
