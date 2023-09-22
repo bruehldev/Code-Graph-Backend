63127539ed77ec4b3ce749d25d01f4d5cecc0d40
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-22 16:29:26 +0200

    feat(plot): add search for code

--- src/plot/router.py
+++ src/plot/router.py
@@ -9,7 +9,6 @@ from database.postgresql import (
     get_segment_table,
     get_reduced_embedding_table,
     get_cluster_table,
-    plot_search_annotion,
     plot_search_cluster,
     plot_search_segment,
 )
@@ -116,7 +115,7 @@ def search_segments_route(
     search_query: str,
     limit: int = 100,
     db: Session = Depends(get_db),
-):
+) -> PlotTable:
     plots = []
     ProjectAlias = aliased(Project)
     SentenceAlias = aliased(Sentence)
@@ -166,20 +165,55 @@ def search_segments_route(
     return {"data": result_dicts, "length": len(result_dicts), "limit": limit}
 
 
-@router.get("/annotation/")
-def search_annoations_route(dataset_name: Dataset_names, model_name: Model_names, query: str, limit: int = 100) -> PlotTable:
-    segment_table_name = get_path_key("segments", dataset_name)
-    segment_table = get_segment_table(segment_table_name)
-    reduced_embedding_table = get_reduced_embedding_table(get_path_key("reduced_embedding", dataset_name, model_name), segment_table_name)
-    cluster_table = get_cluster_table(get_path_key("clusters", dataset_name, model_name), segment_table_name)
+@router.get("/code/")
+def search_code_route(project_id: int, search_code_id: int, limit: int = 100, db: Session = Depends(get_db)) -> PlotTable:
+    plots = []
+    ProjectAlias = aliased(Project)
+    SentenceAlias = aliased(Sentence)
+    SegmentAlias = aliased(Segment)
+    DatasetAlias = aliased(Dataset)
+    ReducedEmbeddingAlias = aliased(ReducedEmbedding)
+    EmbeddingAlias = aliased(Embedding)
+    CodeAlias = aliased(Code)
+    ClusterAlias = aliased(Cluster)
 
-    plots = plot_search_annotion(segment_table, reduced_embedding_table, cluster_table, query, as_dict=True, limit=limit)
+    datasets = db.query(DatasetAlias).filter(DatasetAlias.project_id == project_id).all()
+    dataset_ids = [dataset.dataset_id for dataset in datasets]
+    query = (
+        db.query(
+            ClusterAlias,
+            ReducedEmbeddingAlias,
+            EmbeddingAlias,
+            SegmentAlias,
+            SentenceAlias,
+            CodeAlias,
+            ProjectAlias,
+        )
+        .filter(ProjectAlias.project_id == project_id)
+        .filter(SentenceAlias.dataset_id.in_(dataset_ids))
+        .where(CodeAlias.code_id == search_code_id)
+        .join(ReducedEmbeddingAlias, ClusterAlias.reduced_embedding_id == ReducedEmbeddingAlias.reduced_embedding_id)
+        .join(EmbeddingAlias, ReducedEmbeddingAlias.embedding_id == EmbeddingAlias.embedding_id)
+        .join(SegmentAlias, EmbeddingAlias.segment_id == SegmentAlias.segment_id)
+        .join(SentenceAlias, SegmentAlias.sentence_id == SentenceAlias.sentence_id)
+        .join(CodeAlias, SegmentAlias.code_id == CodeAlias.code_id)
+        .join(ProjectAlias, CodeAlias.project_id == ProjectAlias.project_id)
+        .limit(limit)
+    )
 
-    return {
-        "data": plots,
-        "length": len(plots),
-        "limit": limit,
-    }
+    plots = query.all()
+    result_dicts = [
+        {
+            "id": row[3].segment_id,
+            "sentence": row[4].text,
+            "segment": row[3].text,
+            "code": row[5].code_id,
+            "reduced_embedding": {"x": row[1].pos_x, "y": row[1].pos_y},
+            "cluster": row[0].cluster,
+        }
+        for row in plots
+    ]
+    return {"data": result_dicts, "length": len(result_dicts), "limit": limit}
 
 
 @router.get("/cluster/")
