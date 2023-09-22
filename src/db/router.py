d7d73c89e2dc265fb57ecd3853ba5a886161391f
Daniel Brühl <bruehl.dev@gmail.com>
2023-09-22 22:36:50 +0200

    feat(db): add download routes for files

--- src/db/router.py
+++ src/db/router.py
@@ -1,15 +1,22 @@
-from typing import List
+import os
 
+from typing import List
 from fastapi import APIRouter, Depends
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from pathlib import Path as FilePath
 from sqlalchemy import Column, Integer, MetaData, Table, text
 from sqlalchemy.orm import Session
 
-from db.service import (delete_all_tables, delete_table, get_table_info,
-                        get_table_names, init_db)
+from db.service import delete_all_tables, delete_table, get_table_info, get_table_names, init_db
 from db.session import get_db, get_engine
 
+from utilities.string_operations import get_root_path
+
 router = APIRouter()
+
+# Define the root folder where your files are located
+root_folder_path = get_root_path()
 
 
 @router.get("/tables")
@@ -35,3 +42,28 @@ def get_table_info_route(db: Session = Depends(get_db)):
 @router.delete("/{table_name}")
 async def delete_table_endpoint(table_name: str):
     return {"name": table_name, "deleted": delete_table(table_name)}
+
+
+def list_files_recursive(folder_path):
+    files = []
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for filename in filenames:
+            files.append(os.path.relpath(os.path.join(dirpath, filename), folder_path))
+    return files
+
+
+@router.get("/list-files")
+async def list_all_files():
+    # List all files in the root folder and its subfolders
+    files = list_files_recursive(root_folder_path)
+    return {"files": files}
+
+
+@router.get("/download/{file_path:path}")
+async def download_file(file_path: str):
+    # Ensure the requested file exists
+    file_to_download = root_folder_path / FilePath(file_path)
+    if file_to_download.is_file():
+        return FileResponse(file_to_download)
+    else:
+        return {"error": "File not found"}
