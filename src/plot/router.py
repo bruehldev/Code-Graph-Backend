6a860437f0219821ae5e7870df0e90b6449bc400
Daniel Brühl <bruehl.dev@gmail.com>
2023-10-03 20:02:54 +0200

    feat(plot.router): add search_code_segments_route

--- src/plot/router.py
+++ src/plot/router.py
@@ -265,6 +265,64 @@ def search_clusters_route(project_id: int, search_cluster_id: int, limit: int = 
     return {"data": result_dicts, "length": len(result_dicts), "limit": limit}
 
 
+@router.get("/code/{code_id}/search")
+def search_code_segments_route(
+    project_id: int,
+    code_id: int,
+    search_segment_query: str,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+) -> PlotTable:
+    plots = []
+    ProjectAlias = aliased(Project)
+    SentenceAlias = aliased(Sentence)
+    SegmentAlias = aliased(Segment)
+    DatasetAlias = aliased(Dataset)
+    ReducedEmbeddingAlias = aliased(ReducedEmbedding)
+    EmbeddingAlias = aliased(Embedding)
+    CodeAlias = aliased(Code)
+    ClusterAlias = aliased(Cluster)
+
+    datasets = db.query(DatasetAlias).filter(DatasetAlias.project_id == project_id).all()
+    dataset_ids = [dataset.dataset_id for dataset in datasets]
+    query = (
+        db.query(
+            ClusterAlias,
+            ReducedEmbeddingAlias,
+            EmbeddingAlias,
+            SegmentAlias,
+            SentenceAlias,
+            CodeAlias,
+            ProjectAlias,
+        )
+        .filter(ProjectAlias.project_id == project_id)
+        .filter(SentenceAlias.dataset_id.in_(dataset_ids))
+        .where(CodeAlias.code_id == code_id)
+        .where(SegmentAlias.text_tsv.match(search_segment_query, postgresql_regconfig="english"))
+        .join(ReducedEmbeddingAlias, ClusterAlias.reduced_embedding_id == ReducedEmbeddingAlias.reduced_embedding_id)
+        .join(EmbeddingAlias, ReducedEmbeddingAlias.embedding_id == EmbeddingAlias.embedding_id)
+        .join(SegmentAlias, EmbeddingAlias.segment_id == SegmentAlias.segment_id)
+        .join(SentenceAlias, SegmentAlias.sentence_id == SentenceAlias.sentence_id)
+        .join(CodeAlias, SegmentAlias.code_id == CodeAlias.code_id)
+        .join(ProjectAlias, CodeAlias.project_id == ProjectAlias.project_id)
+        .limit(limit)
+    )
+
+    plots = query.all()
+    result_dicts = [
+        {
+            "id": row[3].segment_id,
+            "sentence": row[4].text,
+            "segment": row[3].text,
+            "code": row[5].code_id,
+            "reduced_embedding": {"x": row[1].pos_x, "y": row[1].pos_y},
+            "cluster": row[0].cluster,
+        }
+        for row in plots
+    ]
+    return {"data": result_dicts, "length": len(result_dicts), "limit": limit}
+
+
 @router.get("/segment")
 def search_segment_route(project_id: int, search_segment_query: str, limit: int = 100, db: Session = Depends(get_db)) -> PlotTable:
     plots = []
